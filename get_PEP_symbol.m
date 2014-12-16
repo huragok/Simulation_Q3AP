d3bd5c7af83fb4023c9c6136fed1365c1ed60d0c
wenhao.wu@Mack <wenhaowu1989@hotmail.com>
2014-12-16 15:59:38 -0800

    Random delta generator halfway there
    Need to help Tian Liang move his matress to Jason's home.

--- get_PEP_symbol.m
+++ get_PEP_symbol.m
@@ -23,7 +23,6 @@ function p = get_PEP_symbol(x, y, mu_h, sigma2_h, sigma2_eps, sigma2_v, N, xi)
 %	Outputs:
 %		p:			scalar, the pairwise error probability of symbols in
 %                   relay - HARQ network
-
 % _____________________________________________________________________________
 % Author: Wenhao Wu
 % Email: wnhwu@ucdavis.edu
@@ -36,10 +35,32 @@ function p = get_PEP_symbol(x, y, mu_h, sigma2_h, sigma2_eps, sigma2_v, N, xi)
 %       CA, 2004.
 % _____________________________________________________________________________
 
+tau = tan(((1 : N)' - 1 / 2) * pi / N);
+omega = xi + 1j * xi * tau;
+
+mu0 = [mu_h(1); 0; 0];
+R0 = diag([sigma2_h(1), sigma2_eps(1), sigma2_v]);
+e0 = x(1) - y(1);
+A0 = [abs(e0)^2, -e0' * y(1), e0';...
+      -e0 * y(1)', abs(y(1))^2 - abs(x(1))^2, e0';...
+      e0, e0, 0];
+
+mu12 = [mu_h(2); mu_h(3); 0; 0; 0];
+R12 = diag([sigma2_h(2), sigma2_h(3), sigma2_eps(2), sigma2_eps(3), sigma2_v]);
+e1 = x(2) - y(2);
+e2 = x(3) - y(3);
+A12 = [abs(e1)^2, e1' * e2, -e1' * y(2), -e1' * y(3), e1';...
+       e1 * e2', abs(e2)^2, -e2' * y(2), -e2' * y(3), e2';...
+       -e1 * y(2)', -e2 * y(2)', abs(y(2))^2 - abs(x(2))^2, y(2)'*y(3) - x(2)'*x(3), e1';...
+       -e1 * y(3)', -e2 * y(3)', y(2)*y(3)' - x(2)*x(3)', abs(y(3))^2 - abs(x(3))^2, e2';...
+       e1, e2, e1, e2, 0];
+   
+psi = get_Psi_Gaussian(omega, mu0, R0, A0) .* get_Psi_Gaussian(omega, mu12, R12, A12);
+p = sum(real(psi) + tau .* imag(psi)) / (2 * N);
 end
 
-function psi = get_Psi_Gaussian(omega, mu, Sigma, A)
-%   psi = get_Psi_Gaussian(omega, mu, Sigma, A)
+function psi = get_Psi_Gaussian(omega, mu, R, A)
+%   psi = get_Psi_Gaussian(omega, mu, R, A)
 %   Compute the moment generating function (MGF) for the quadaratic form of
 %   Gaussian random variables z'Az.
 % _____________________________________________________________________________
@@ -49,11 +70,18 @@ function psi = get_Psi_Gaussian(omega, mu, Sigma, A)
 % 		
 %       mu:         d-by-1 vector, the mean value of the Gaussian variable
 %                   vector z
-%       Sigma:      d-by-d positive semi-definite matrix, the covariance
+%       R:      d-by-d positive semi-definite matrix, the covariance
 %                   matrix of the Gaussian variable vector z
 %       A:          a d-by-d Hermitian matrix
 %	Outputs:
 %		psi:		N-by-1 vector, a sequence of MGF values evaluated
 % _____________________________________________________________________________
+
+d = length(mu); % Size of the Gaussian random vector
+psi = zeros(N, 1);
+for n = 1 : N
+    IRA = eye(d) + omega(n) * R * A;
+    psi(n) = exp(-omega(n) * mu' * A / IRA * mu) / det(IRA);
+end
 
 end
