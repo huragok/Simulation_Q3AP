428fcf2ab3611e3845f509af25102c093b1fe04a
wenhaowu@Loki <wenhaowu1989@hotmail.com>
2015-01-30 20:49:26 -0800

    Start to rewrite the cost matrix generation function with c++.

---
+++ cpp/Test.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <pthread.h>
+#include "relayHARQ.h"
+
+int main()
+{
+    std::cout << relayHARQ::QAM4[0];
+    return 0;
+}
---
+++ cpp/relayHARQ.cpp
@@ -0,0 +1,17 @@
+/*
+ *  \author    Wenhao Wu
+ *  \version   0.1
+ *  \date      2015-01-30  
+ */
+
+#include "relayHARQ.h"
+
+double relayHARQ::get_PEP_symbol(arma::cx_vec x, arma::cx_vec y, arma::vec mu_h, arma::cx_mat sigma2_h, arma::cx_mat sigma2_eps, arma::cx_mat sigma2_v, int N, double xi)
+{
+    return 0;
+}
+
+arma::umat get_n_diff_bits(int Nbps)
+{
+	return arma::umat();
+}
