6c359313ab1ebedc9cabdcfb366ae66a94f368ef
wenhaowu@Loki <wenhaowu1989@hotmail.com>
2015-01-31 22:38:23 -0800

    Rewrite all low-level function.
    Using armadillo off the reservation.

--- cpp/Test.cpp
+++ cpp/Test.cpp
@@ -4,6 +4,35 @@
 
 int main()
 {
-    std::cout << relayHARQ::QAM4[0];
-    return 0;
+	/*
+	arma::umat B = relayHARQ::get_n_diff_bits(4);
+	for (int i = 0; i < 16; i++)
+	{
+		for (int j = 0; j < 16; j++)
+		{
+			std::cout << B(i, j) << std::endl;
+		}
+	}
+	*/
+
+
+	arma::vec x = arma::tan((arma::linspace(1, 10, 10) - 0.5) * M_PI / 10) ;
+    for (int i = 0; i < 10; i++)
+    {
+    	std::cout << x(i) << std::endl;
+    }
+
+    std::complex<double> j(0, 1);
+	arma::cx_vec omega = 0.25 * (1 + x * std::complex<double>(0, 1));
+
+	for (int i = 0; i < 10; i++)
+	{
+		std::cout << omega(i) << std::endl;
+	}
+
+	arma::cx_mat y(3, 3, arma::fill::zeros);
+	y(0, 0) = pow(abs(conj(std::complex<double>(3, 4))), 2);
+	std::cout << y(0, 0) << std::endl;
+
+	return 0;
 }
--- cpp/relayHARQ.cpp
+++ cpp/relayHARQ.cpp
@@ -6,12 +6,115 @@
 
 #include "relayHARQ.h"
 
-double relayHARQ::get_PEP_symbol(arma::cx_vec x, arma::cx_vec y, arma::vec mu_h, arma::cx_mat sigma2_h, arma::cx_mat sigma2_eps, arma::cx_mat sigma2_v, int N, double xi)
-{
-    return 0;
+namespace relayHARQ{
+
+	arma::cx_vec get_Psi_Gaussian(arma::cx_vec omega, arma::cx_vec mu, arma::cx_mat R, arma::cx_mat A)
+	{
+		unsigned int N = omega.n_elem; // Size of serial expansion
+		unsigned int d = mu.n_elem; // Size of the Gaussian random vector
+		arma::cx_vec psi(N, arma::fill::zeros);
+		arma::cx_mat eye_d(d, d, arma::fill::eye); // d-by-d identity matrix
+		for (unsigned int n = 0; n < N; n++)
+		{
+			arma::cx_mat IRA = eye_d + omega(n) * R * A;
+			arma::cx_mat temp = -omega(n) * mu.t() * A / IRA * mu;
+			psi(n) = std::exp(temp(0)) / arma::det(IRA);
+		}
+		return psi;
+	}
+
+	double get_PEP_symbol(arma::cx_vec x, arma::cx_vec y, arma::cx_vec mu_h, arma::cx_vec sigma2_h, arma::cx_vec sigma2_eps, double sigma2_v, int N, double xi)
+	{
+		arma::vec tau = arma::tan((arma::linspace(1, N, N) - 0.5) * M_PI / N);
+		arma::cx_vec omega = xi * (1 + tau * std::complex<double>(0, 1));
+
+		// Phase 1
+		arma::cx_vec mu0(3, arma::fill::zeros);
+		mu0(0) = mu_h(0);
+
+		arma::cx_mat R0(3, 3, arma::fill::zeros);
+		R0(0, 0) = sigma2_h(0);
+		R0(1, 1) = sigma2_eps(0);
+		R0(2, 2) = sigma2_v;
+
+		std::complex<double> e0 = x(0) - y(0);
+
+		arma::cx_mat A0(3, 3, arma::fill::zeros);
+		A0(0, 0) = pow(abs(e0), 2);
+		A0(0, 1) = -conj(e0) * y(0);
+		A0(0, 2) = conj(e0);
+		A0(1, 0) = conj(A0(0, 1));
+		A0(1, 1) = pow(abs(y(0)), 2) - pow(abs(x(0)), 2);
+		A0(1, 2) = conj(e0);
+		A0(2, 0) = conj(A0(0, 2));
+		A0(2, 1) = conj(A0(1, 2));
+
+		// Phase 2
+		arma::cx_vec mu12(5, arma::fill::zeros);
+		mu12(0) = mu_h(1);
+		mu12(1) = mu_h(2);
+
+		arma::cx_mat R12(5, 5, arma::fill::zeros);
+		R12(0, 0) = sigma2_h(1);
+		R12(1, 1) = sigma2_h(2);
+		R12(2, 2) = sigma2_eps(1);
+		R12(3, 3) = sigma2_eps(2);
+		R12(4, 4) = sigma2_v;
+
+		std::complex<double> e1 = x(1) - y(1);
+		std::complex<double> e2 = x(2) - y(2);
+
+		arma::cx_mat A12(5, 5, arma::fill::zeros);
+		A12(0, 0) = pow(abs(e1), 2);
+		A12(0, 1) = conj(e1) * e2;
+		A12(0, 2) = -conj(e1) * y(1);
+		A12(0, 3) = -conj(e1) * y(2);
+		A12(0, 4) = conj(e1);
+		A12(1, 0) = conj(A12(0, 1));
+		A12(1, 1) = pow(abs(e2), 2);
+		A12(1, 2) = -conj(e2) * y(1);
+		A12(1, 3) = -conj(e2) * y(2);
+		A12(1, 4) = conj(e2);
+		A12(2, 0) = conj(A12(0, 2));
+		A12(2, 1) = conj(A12(1, 2));
+		A12(2, 2) = pow(abs(y(1)), 2) - pow(abs(x(1)), 2);
+		A12(2, 3) = conj(y(1)) * y(2) - conj(x(1)) * x(2);
+		A12(2, 4) = conj(e1);
+		A12(3, 0) = conj(A12(0, 3));
+		A12(3, 1) = conj(A12(1, 3));
+		A12(3, 2) = conj(A12(2, 3));
+		A12(3, 3) = pow(abs(y(2)), 2) - pow(abs(x(2)), 2);
+		A12(3, 4) = conj(e2);
+		A12(4, 0) = conj(A12(0, 4));
+		A12(4, 1) = conj(A12(1, 4));
+		A12(4, 2) = conj(A12(2, 4));
+		A12(4, 3) = conj(A12(3, 4));
+
+		arma::cx_vec psi = get_Psi_Gaussian(omega, mu0, R0, A0) % get_Psi_Gaussian(omega, mu12, R12, A12);
+
+		double p = sum(real(psi) + tau % imag(psi)) / (2 * N);
+
+		return p;
+	}
+
+	arma::umat get_n_diff_bits(unsigned int Nbps)
+	{
+		unsigned int Q = (unsigned int)(pow(2, Nbps));
+		arma::umat B(Q, Q);
+		for (unsigned int i = 0; i < Q; i++)
+		{
+			for (unsigned int j = 0; j < Q; j++)
+			{
+				unsigned int diff = i ^ j;
+
+				for (B(i, j) = 0; diff; diff >>= 1)
+				{
+					B(i, j) += diff & 1;
+				}
+			}
+		}
+		return B;
+	}
+
 }
 
-arma::umat get_n_diff_bits(int Nbps)
-{
-	return arma::umat();
-}
